343ddf02f6a237695a32787dc14557b155c324cf
Vinpasso <vinpasso@vinpasso.org>
2015-01-19 17:29:22 +0100

    Sync System Exit to max 60 Seconds

--- src/main/java/bridgempp/BridgeMPP.java
+++ src/main/java/bridgempp/BridgeMPP.java
@@ -16,41 +16,67 @@ import java.util.logging.Logger;
  * @author Vinpasso
  */
 public class BridgeMPP {
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        ShadowManager.log(Level.INFO, "Server startup commencing...");
-        ConfigurationManager.initializeConfiguration();
-        PermissionsManager.loadAccessKeys();
-        ServiceManager.loadAllServices();
-        GroupManager.loadAllGroups();
-        ShadowManager.log(Level.INFO, "Server Initialization completed");
-    }
-    
-    public static String getPathLocation()
-    {
-        try {
-            URL url = BridgeMPP.class.getProtectionDomain().getCodeSource().getLocation();
-            String path =  new File(url.toURI()).getPath();
-            if(path.endsWith(".jar"))
-            {
-                path = path.substring(0, path.lastIndexOf("/"));
-            }
-            return path;
-        } catch (URISyntaxException ex) {
-            Logger.getLogger(BridgeMPP.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
-    
-    public static void exit()
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		ShadowManager.log(Level.INFO, "Server startup commencing...");
+		ConfigurationManager.initializeConfiguration();
+		PermissionsManager.loadAccessKeys();
+		ServiceManager.loadAllServices();
+		GroupManager.loadAllGroups();
+		ShadowManager.log(Level.INFO, "Server Initialization completed");
+	}
+
+	public static String getPathLocation() {
+		try {
+			URL url = BridgeMPP.class.getProtectionDomain().getCodeSource()
+					.getLocation();
+			String path = new File(url.toURI()).getPath();
+			if (path.endsWith(".jar")) {
+				path = path.substring(0, path.lastIndexOf("/"));
+			}
+			return path;
+		} catch (URISyntaxException ex) {
+			Logger.getLogger(BridgeMPP.class.getName()).log(Level.SEVERE, null,
+					ex);
+		}
+		return null;
+	}
+
+	public static void exit()
     {
         ShadowManager.log(Level.INFO, "Server shutdown commencing...");
         PermissionsManager.saveAccessKeys();
         GroupManager.saveAllGroups();
         ServiceManager.unloadAllServices();
         ShadowManager.log(Level.INFO, "Server shutdown completed");
+        ShadowManager.log(Level.INFO, "Syncing System Exit (60 Seconds)");
+        long syncTime = 0;
+        	for(Thread thread : Thread.getAllStackTraces().keySet())
+        	{
+        		if(syncTime > 60000)
+        		{
+        			break;
+        		}
+        		if(thread.equals(Thread.currentThread()))
+        		{
+        			continue;
+        		}
+        		if(thread.isAlive())
+        		{
+        			long startTime = System.currentTimeMillis();
+        			try {
+						thread.join(60000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+        			syncTime += System.currentTimeMillis() - startTime;
+        		}
+        	}
+        ShadowManager.log(Level.INFO, "Killing Process");
+        System.exit(0);
     }
 }
