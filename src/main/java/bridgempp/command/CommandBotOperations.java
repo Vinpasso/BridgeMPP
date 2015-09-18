e20603957922c4ecf4301d9fb0e88f6739ae730b
Vinpasso <vinpasso@vinpasso.org>
2015-09-18 16:26:01 +0200

    Added Bot usekey command

--- src/main/java/bridgempp/command/CommandBotOperations.java
+++ src/main/java/bridgempp/command/CommandBotOperations.java
@@ -3,6 +3,7 @@ package bridgempp.command;
 import java.util.logging.Level;
 
 import bridgempp.Message;
+import bridgempp.PermissionsManager;
 import bridgempp.ShadowManager;
 import bridgempp.PermissionsManager.Permission;
 import bridgempp.command.wrapper.CommandName;
@@ -25,7 +26,7 @@ public class CommandBotOperations
 	
 	@CommandName("!botsubscribegroup: Join a group")
 	@CommandTrigger("!botsubscribegroup")
-	@HelpTopic("Subscribe the Message's Sender to the specified Group with name. Does nosennconfirmation messages.")
+	@HelpTopic("Subscribe the Message's Sender to the specified Group with name. Does not send confirmation messages.")
 	@RequiredPermission(Permission.SUBSCRIBE_UNSUBSCRIBE_GROUP)
 	public static void cmdSubscribeGroup(String name, Message message)
 	{
@@ -36,6 +37,21 @@ public class CommandBotOperations
 		} else
 		{
 			message.getOrigin().sendOperatorMessage("Error: Group not found");
+		}
+	}
+	
+	@CommandName("!botusekey: Use a BridgeMPP access key")
+	@CommandTrigger("!botusekey")
+	@HelpTopic("Uses a BridgeMPP Access key to give the Sender of the message the key's rights. Does not send confirmation messages.")
+	public static void cmdUseKey(String key, Message message)
+	{
+		boolean success = PermissionsManager.useKey(key, message.getOrigin());
+		if (success)
+		{
+			ShadowManager.log(Level.INFO, message.getOrigin().toString() + " has used key and now has " + message.getOrigin().getPermissions());
+		} else
+		{
+			message.getOrigin().sendOperatorMessage("Key Failure. Incorrect Key");
 		}
 	}
 	
