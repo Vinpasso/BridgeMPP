1bdced611c87550ff8f79fbf5f50049a87d6c9a2
Vinpasso <vinpasso@vinpasso.org>
2015-10-04 10:54:43 +0200

    WIP: EndpointHandleCreator

---
+++ src/main/java/bridgempp/state/endpoint/EndpointHandleCreator.java
@@ -0,0 +1,22 @@
+package bridgempp.state.endpoint;
+
+import bridgempp.data.Endpoint;
+import bridgempp.service.SingleToMultiBridgeService;
+import bridgempp.state.EventListener;
+import bridgempp.state.EventManager.Event;
+import bridgempp.state.EventSubscribe;
+
+@EventSubscribe(Event.ENDPOINT_CREATED)
+public class EndpointHandleCreator extends EventListener<Endpoint>
+{
+
+	public void onEvent(Endpoint endpoint)
+	{
+		if(endpoint.getService() instanceof SingleToMultiBridgeService<?, ?>)
+		{
+			SingleToMultiBridgeService<?, ?> service = (SingleToMultiBridgeService<?, ?>) endpoint.getService();
+			//TODO: Work in Progress: Automatic Handle creation
+		}
+	}
+	
+}
