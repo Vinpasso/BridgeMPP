1562eff7e87aa8a2944530b71ba44ebe87029aa5
Vinpasso <vincent@jd-bode.de>
2019-02-15 23:05:10 +0100

    Update telegram import to fix build

--- src/main/java/bridgempp/services/telegram/TelegramBot.java
+++ src/main/java/bridgempp/services/telegram/TelegramBot.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import java.util.logging.Level;
 
 import org.apache.commons.io.IOUtils;
-import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.api.methods.GetFile;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
